9fc8ff4d18c3ee4c33ff5029e307276cd7f87d82
agmorev <agmorev@gmail.com>
2020-06-03 01:10:06 +0300

    Email contacts

--- contacts/views.py
+++ contacts/views.py
@@ -47,7 +47,7 @@ class ContactFormView(FormView):
                 subject,
                 SafeString(contact_message),
                 contact_email,
-                ['info@icustoms.info', ],
+                ['contacts@icustoms.info', ],
             )
             msg.content_subtype = "html"
             msg.send()
