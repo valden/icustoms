88fe42f186ff221db654f06590d334f22308415b
Denys Valchuk <dvalchuk@gmail.com>
2020-07-10 16:46:37 +0000

    added static files handling

--- core/settings.py
+++ core/settings.py
@@ -140,7 +140,7 @@ DATETIME_FORMAT = 'd.m.Y H:i'
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [STATIC_DIR, ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, '/static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 WHITENOISE_MANIFEST_STRICT = False
 
