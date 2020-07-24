5b0e587bb17e8714ca529224807d307d798ad9cf
Denys Valchuk <dvalchuk@gmail.com>
2020-07-24 23:16:44 +0000

    final migration

--- core/settings.py
+++ core/settings.py
@@ -94,10 +94,21 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'icustoms',
+        'USER': 'icadmin',
+        'PASSWORD': 'test11',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
 
